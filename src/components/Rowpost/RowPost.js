58fddbaccbd7436c735658bc33c1d1663ea7b006
Muhammed Fayis Bin Faisal <116888738+mfayis@users.noreply.github.com>
2023-03-20 01:02:20 +0530

    added autoplay

--- src/components/Rowpost/RowPost.js
+++ src/components/Rowpost/RowPost.js
@@ -22,6 +22,10 @@ function RowPost(props) {
   const opts = {
     height: '390',
     width: '100%',
+     playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }
     
   };
   
